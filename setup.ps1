dc1f5ed419b49f665880ef623e7eb49583bf1421
tonytech83 <a.petrov.83@gmail.com>
2025-08-21 14:39:07 +0300

    Fix the issue with installing nerd font

--- setup.ps1
+++ setup.ps1
@@ -4,45 +4,67 @@ if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
   break
 }
 
-function Install-Profile {
-  # Profile creation or update
-  if (!(Test-Path -Path $PROFILE -PathType Leaf)) {
-    try {
-      # Detect Version of PowerShell & Create Profile directories if they do not exist.
-      $profilePath = ""
-      if ($PSVersionTable.PSEdition -eq "Core") {
-        $profilePath = "$env:userprofile\Documents\Powershell"
-      }
-      elseif ($PSVersionTable.PSEdition -eq "Desktop") {
-        $profilePath = "$env:userprofile\Documents\WindowsPowerShell"
-      }
+function Install-NerdFont {
+  param (
+      [string]$FontName = "JetBrainsMono",
+      [string]$FontDisplayName = "JetBrainsMono NF",
+      [string]$Version = "3.2.1"
+  )
 
-      # Check if profilePath exists, if not create it
-      if (!(Test-Path -Path $profilePath)) {
-        New-Item -Path $profilePath -ItemType "directory"
-      }
-
-      Invoke-RestMethod https://github.com/tonytech83/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -OutFile $PROFILE
-      Write-Host "The profile @ [$PROFILE] has been created."
-    }
-    catch {
-      Write-Error "Failed to create the profile. Error: $_"
-    }
-  }
-  else {
-    # PROFILE already exists
-    try {
-      $backupPath = Join-Path (Split-Path $PROFILE) "oldprofile.ps1"
-      Move-Item -Path $PROFILE -Destination $backupPath -Force
-      Invoke-RestMethod https://github.com/tonytech83/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -OutFile $PROFILE
-      Write-Host "PowerShell profile at [$PROFILE] has been updated."
-      Write-Host "Your old profile has been backed up to [$backupPath]"
-    }
-    catch {
-      Write-Error "Failed to backup and update the profile. Error: $_"
-    }
+  # Installed Font Families
+  $installed = (New-Object System.Drawing.Text.InstalledFontCollection).Families.Name
+  if ($installed -contains $FontDisplayName) {
+      Write-Host "Font '$FontDisplayName' already installed"
+      return
   }
 
+
+  # Define the URL for the JetBrainsMono Nerd Font zip file
+  $fontUrl = "https://github.com/ryanoasis/nerd-fonts/releases/download/v${Version}/${FontName}.zip"
+  
+  # Define the path where the zip file will be downloaded
+  $tempZipPath = "$env:TEMP\JetBrainsMono.zip"
+  
+  # Define the directory where the font files will be extracted
+  $extractPath = "$env:TEMP\JetBrainsMono"
+
+  # Define the fonts installation path
+  $fontsPath = "$env:WINDIR\Fonts"
+
+  try {
+      # Download the font zip file
+      Invoke-WebRequest -Uri $fontUrl -OutFile $tempZipPath
+      if (-not (Test-Path $tempZipPath)) { throw "Font download failed" }
+
+      # Create a directory to extract the fonts
+      if (-not (Test-Path -Path $extractPath)) {
+          New-Item -ItemType Directory -Path $extractPath | Out-Null
+      } 
+
+      # Extract the zip file
+      Expand-Archive -Path $tempZipPath -DestinationPath $extractPath -Force
+
+      # Get the list of font files from the extracted directory
+      $fontFiles = Get-ChildItem -Path $extractPath -Filter "*.ttf"
+      if (-not $fontFiles) { throw "No font files found in archive." }
+
+      foreach ($Font in $fontFiles) {
+          # Copy to C:\Windows\Fonts
+          Copy-Item $Font $fontsPath
+          New-ItemProperty -Name $Font.BaseName -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -PropertyType string -Value $Font.name
+      }
+  
+      # Clean up
+      Remove-Item -Path $tempZipPath -Force
+      Remove-Item -Path $extractPath -Recurse -Force
+
+      Write-Output "${FontDisplayName} font has been installed successfully."
+  }
+  catch {
+      Write-Error "Failed to install ${FontDisplayName}. Error: $_"
+      return
+  }
+  
 }
 
 function Install-NerdFont {
@@ -51,9 +73,6 @@ function Install-NerdFont {
     [string]$FontDisplayName = "JetBrainsMono NF",
     [string]$Version = "3.2.1"
   )
-
-  # Installed Font Families
-  $fontFamilies = (New-Object System.Drawing.Text.InstalledFontCollection).Families.Name
 
   # Installed Font Families
   $fontFamilies = (New-Object System.Drawing.Text.InstalledFontCollection).Families.Name
